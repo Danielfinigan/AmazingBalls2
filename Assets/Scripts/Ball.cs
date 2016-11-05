eea44128a137338503261da6b63792c8ff2f1d21
ian <ianyoung37@yahoo.com>
2016-11-05 15:25:42 -0400

    started KillTrigger

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public static Ball Instance;
+	public RigidBody rb = new RigidBody ();
+
+	public void Awake () {
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
