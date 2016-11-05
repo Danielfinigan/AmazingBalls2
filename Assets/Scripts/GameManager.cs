0551f8e970df5a874adff55d7001e13ab4960090
ian <ianyoung37@yahoo.com>
2016-11-05 15:03:31 -0400

    added to GameManager start button works now

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public enum GameState {
+	start,
+	inGame,
+	gameOver,
+	levelComplete,
+	youWin
+}
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public static GameManager Instance;
+	public GameState currentGameState = GameState.start;
+
+	public GameObject StartScreen;
+
+	public void Awake () {
+		Instance = this;
+	}
+
+	public void StartGame() {
+		SetGameState (GameState.inGame);
+	}
+
+	void SetGameState (GameState newGameState) {
+		if (newGameState == GameState.start) {
+			StartScreen.SetActive(true);
+		} else if (newGameState == GameState.inGame) {
+			StartScreen.SetActive (false);
+		} else if (newGameState == GameState.levelComplete) {
+			StartScreen.SetActive (false);
+		} else if (newGameState == GameState.gameOver) {
+			StartScreen.SetActive (false);
+		} else if (newGameState == GameState.youWin) {
+			StartScreen.SetActive (false);
+		}
+		currentGameState = newGameState;
 	}
 	
 	// Update is called once per frame
