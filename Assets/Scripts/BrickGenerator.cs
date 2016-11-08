6703d6dd51e263af1da21352dcbd6044c7ff0ff2
Dan Finigan <danielfinigan@gmail.com>
2016-11-07 21:32:00 -0500

    Bricks now produrally generated in pretty colors

---
+++ Assets/Scripts/BrickGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BrickGenerator : MonoBehaviour {
+    public static BrickGenerator Instance;
+    public List<Brick> brickPrebabs = new List<Brick>();
+    public List<Brick> bricks = new List<Brick>();
+
+    private int _brickPicker = 0;
+	
+	// Update is called once per frame
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        buildLevel();
+    }
+
+    public void AddBrick(float xPos, float yPos)
+    {
+        Brick brick = (Brick)Instantiate(brickPrebabs[_brickPicker]);
+        Vector2 spawnPosition = new Vector2(xPos, yPos);
+        brick.transform.position = spawnPosition;
+        bricks.Add(brick);
+        _brickPicker = (_brickPicker + 1) % 3;
+    }
+
+    public void buildLevel()
+    {
+        for(int y = -1; y < 26; y += 2)
+        {
+            for (int x = -40; x < 41; x += 5)
+            {
+                AddBrick(x, y);
+            }
+        }
+    }
+}
--- Assets/Scripts/KillTrigger.cs
+++ Assets/Scripts/KillTrigger.cs
@@ -5,13 +5,10 @@ public class KillTrigger : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
-        Debug.Log("Killed");
         if (other.tag == "Ball")
 		{
-            Debug.Log("Ball tag");
 			GameManager.Instance.GameOver();
 			Ball2.Instance.rb.constraints = RigidbodyConstraints2D.FreezePosition;
-			Debug.Log("Freeze Constraints");
 		}
 	}
 }
